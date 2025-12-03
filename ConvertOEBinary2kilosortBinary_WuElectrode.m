f8a4961d04b4b54efa15ec50f37f62fa18656028
Xin Hao <126965580+SheenHao8023@users.noreply.github.com>
2025-12-03 16:18:19 +0800

    Add files via upload

---
+++ ConvertOEBinary2kilosortBinary_WuElectrode.m
@@ -0,0 +1,178 @@
+clear
+addpath(genpath('C:\Kilosort2-user\my_kilosort'));
+datapath={
+    'C:\Users\XinHao\Desktop\DiffLearn_MultiRegion\Ephys\CIBRXH027\2025-11-25_12-29-37\';... 
+    'C:\Users\XinHao\Desktop\DiffLearn_MultiRegion\Ephys\CIBRXH027\2025-11-26_10-05-43\';... 
+    'C:\Users\XinHao\Desktop\DiffLearn_MultiRegion\Ephys\CIBRXH027\2025-11-27_10-06-00\';... 
+    'C:\Users\XinHao\Desktop\DiffLearn_MultiRegion\Ephys\CIBRXH027\2025-11-28_12-27-55\';... 
+};
+% Laser_trial={
+%     [1 2 3];...
+%     [1 2 3];...
+%     [1 2 3];...
+%     [1 2 3];...
+%     }; %sti right/left/bilat
+
+for k = 1:length(datapath)
+    file_dir = datapath{k};
+    i_str=strfind(file_dir,'\');
+    % laser_trial = Laser_trial{k};
+
+    probe = 1; %region 1 intan headstage 1-64 ch;region 2 intan headstage 65-128 ch etc for mutiple arrays in different regions
+    shank = 1; 
+    mmapN=1;
+    for probe_tmp=1:probe
+        for shank_tmp=1:shank
+            if ~exist([file_dir,'Kilosort_data',num2str(probe_tmp),num2str(shank_tmp)]) 
+                mkdir([file_dir,'Kilosort_data',num2str(probe_tmp),num2str(shank_tmp)])
+            end
+        end
+    end
+    func_process_voltage_traces_kilosort_OE_CB64(file_dir, probe, shank, mmapN);
+    % master_kilosort_WuElectrode; 
+end
+
+%% Local func: func_process_voltage_traces_kilosort_OE_CB64
+function func_process_voltage_traces_kilosort_OE_CB64(file_dir, probe, shank, mmapN)
+    oebin_file = dir(fullfile(file_dir, '**', 'structure.oebin'));
+    oebin_path = fullfile(oebin_file(1).folder, oebin_file(1).name);
+    D = load_open_ephys_binary(oebin_path, 'continuous', mmapN, 'mmap');
+    fs=D.Header.sample_rate;
+    if D.Header.num_channels == 72 % 记录了全部通道，则删除17~48以外的无效通道
+        delete_list = [ ...
+            arrayfun(@(x) sprintf('CH%d', x), 1:16,  'UniformOutput', false), ...
+            arrayfun(@(x) sprintf('CH%d', x), 49:64, 'UniformOutput', false) ...
+        ];
+        idx_del = ismember({D.Header.channels.channel_name}, delete_list);
+        D.Header.channels(idx_del) = [];
+        D.Header.num_channels = numel(D.Header.channels);
+        D.mapped_copy = int16(D.Data.Data(1).mapped);
+        D.mapped_copy(idx_del, :) = [];
+    else
+        D.mapped_copy = D.Data.Data(1).mapped;
+    end
+    probe_ch = D.Header.num_channels / probe;  
+    target_channels = arrayfun(@(x) ['CH' num2str(x)], 17:48, 'UniformOutput', false);
+    channel_idx = find(ismember({D.Header.channels.channel_name}, target_channels));
+
+    wavesurfer_data= D.mapped_copy(D.Header.num_channels-7,:); % BNC1 ch65
+    bitcode_data= D.mapped_copy(D.Header.num_channels-6,:); % BNC2 ch66
+    Ephystrig_data= D.mapped_copy(D.Header.num_channels-5,:); % BNC3 ch67, intan trigger
+    LickingLeft_data = D.mapped_copy(D.Header.num_channels-2,:); % BNC6 (需要check)
+    LickingRight_data = D.mapped_copy(D.Header.num_channels-1,:); % BNC7
+
+    % trial_on_tmp= find(diff(wavesurfer_data)>6000)+1;
+    % trial_on(1)=trial_on_tmp(1);
+    % trial_on=[trial_on(1) trial_on_tmp(find(diff(trial_on_tmp)>40000)+1)];
+    % Ephystrig_data=Ephystrig_data>4000;
+    % trial_on_tmp=find(diff(Ephystrig_data)==1)+1;
+    % trial_off_tmp=find(diff(Ephystrig_data)==-1)+1;
+    % trial_off=[trial_off_tmp(find(abs(trial_off_tmp(1:end-1)-trial_on_tmp(2:end))>0.5*fs)) trial_off_tmp(end)];
+
+    wavesurfer_data=wavesurfer_data*D.Header.channels(end).bit_volts;
+    bitcode_data=bitcode_data*D.Header.channels(end).bit_volts;
+    Ephystrig_data=Ephystrig_data*D.Header.channels(end).bit_volts;
+    LickingLeft_data = LickingLeft_data*D.Header.channels(end).bit_volts;
+    LickingRight_data = LickingRight_data*D.Header.channels(end).bit_volts;
+
+    trial_on_tmp= find(diff(Ephystrig_data>1.5)==1)+1;
+    trial_on(1)=trial_on_tmp(1);
+    trial_on=[trial_on(1) trial_on_tmp(find(diff(trial_on_tmp)>2*fs)+1)];
+    trial_off_tmp= find(diff(Ephystrig_data>1.5)==-1)+1;
+    trial_off(1)=trial_off_tmp(1);
+    trial_off=[trial_off(1) trial_off_tmp(find(diff(trial_off_tmp)>2*fs)+1)];
+
+    for probe_tmp=1:probe
+        for shank_tmp=1:shank
+            TrialNum_prev = 0;
+            TrialNum = 0;
+            Trial_time_all=[];
+            file_dir_output=[file_dir,'kilosort_data',num2str(probe_tmp),num2str(shank_tmp),'\'];
+            output_file_kilosort= [file_dir_output,'kilosort_probe',num2str(probe_tmp),'shank',num2str(shank_tmp),'_data.bin'];
+            fidout = fopen(output_file_kilosort, 'w'); 
+            for trial_k=1:length(trial_on)
+                if trial_k==1
+                    sample_point=1:trial_off(trial_k);       
+                elseif trial_k>1&& trial_k<length(trial_on)
+                    sample_point=trial_off(trial_k-1)+1:trial_off(trial_k);
+                else
+                    sample_point=trial_off(trial_k-1)+1:length(wavesurfer_data);
+                    if length(sample_point)/fs>301 
+                        sample_point=trial_off(trial_k-1)+1:trial_off(trial_k-1)+300*fs; 
+                    end
+                end
+                TimeStamps=(sample_point-trial_on(trial_k))/fs;
+                bitcode_trial=bitcode_data(sample_point);
+
+                TrialNum_prev = TrialNum;
+                try
+                    TrialNum = func_read_bitcode(bitcode_trial,TimeStamps);
+                    disp(['done!  Matched to solo trial #',num2str(TrialNum)]);
+                catch
+                    TrialNum = TrialNum_prev+1;
+                    warning(['Bitcode Failed!  Assigned to solo trial #',num2str(TrialNum)]);
+                end
+
+                Trial_time_tmp=[TimeStamps;ones(1,length(TimeStamps))*TrialNum];
+                Trial_time_all=[Trial_time_all Trial_time_tmp];
+    
+                ch_MUA = [];
+                ch_MUA = D.mapped_copy((probe_tmp-1)*probe_ch+1:probe_tmp*probe_ch,sample_point)*D.Header.channels(1).bit_volts;
+                ch_MUA = double(ch_MUA');
+                ch_MUA = ch_MUA(:,(shank_tmp-1)*32+1:shank_tmp*32); %shank_ch = 32
+                for i_ch = 1:length(channel_idx) % bandpass filter
+                    ch_tmp = timeseries(ch_MUA(:, channel_idx(i_ch)),TimeStamps);
+                    ch_tmp_MUA = idealfilter(ch_tmp, [300 6000], 'pass');
+                    ch_MUA(:, channel_idx(i_ch)) = ch_tmp_MUA.data;
+                end
+
+                % substract off common noise
+                commonNoise = trimmean(ch_MUA,40,2);
+                i_noise=[find(commonNoise>150);find(commonNoise<-200)]; 
+                t_sample=1.57;
+                for i_ch=1:length(channel_idx) % change the time here acorrding to your behavior and photostim
+                    t_post_stim = [0 t_sample+1.3]; % 
+                    i_post_stim = find(TimeStamps>t_post_stim(1) & TimeStamps<t_post_stim(2));
+                    idx1=i_post_stim(end);
+                    X = [ones(size(commonNoise(i_post_stim),1),1) commonNoise(i_post_stim)];
+                    b1 = regress(ch_MUA(i_post_stim,i_ch),X);
+                    t_post_stim = [t_sample+1.3 t_sample+2.1]; 
+                    i_post_stim = find(TimeStamps>t_post_stim(1) & TimeStamps<t_post_stim(2));
+                    idx2=i_post_stim(end);
+                    X = [ones(size(commonNoise(i_post_stim),1),1) commonNoise(i_post_stim)];
+                    b2 = regress(ch_MUA(i_post_stim,i_ch),X);
+                    t_post_stim = [t_sample+2.1 t_sample+4.5]; 
+                    i_post_stim = find(TimeStamps>t_post_stim(1) & TimeStamps<t_post_stim(2));
+                    X = [ones(size(commonNoise(i_post_stim),1),1) commonNoise(i_post_stim)];
+                    b3 = regress(ch_MUA(i_post_stim,i_ch),X);
+                    ch_MUA(:,i_ch) = ch_MUA(:,i_ch) - [commonNoise(1:idx1)*b1(2);commonNoise(idx1+1:idx2)*b2(2);commonNoise(idx2+1:end)*b3(2)];
+                end
+                if ~isempty(i_noise)
+                    for j=1:length(i_noise)
+                        if i_noise(j)>1 && i_noise(j)<length(commonNoise)-1
+                            ch_MUA(i_noise(j)-1:i_noise(j)+1,:)=zeros(3,size(ch_MUA,2));
+                        end
+                    end
+                end
+                clear commonNoise
+
+                % if ismember(TrialNum,laser_trial)
+                %      output_file_name = [file_dir_output,'raw_trace_probe',num2str(probe_tmp),'shank',num2str(shank_tmp),'_laser_trial_',num2str(TrialNum),'.mat'];
+                %      disp(['saving laser trial: ',output_file_name]);
+                %      save(output_file_name,'ch_MUA','TimeStamps','-v7.3');
+                % end
+                % disp(['write: trial ',num2str(TrialNum)]);
+                ch_MUA = int16(ch_MUA');
+                fwrite(fidout, ch_MUA, 'int16');
+    
+                clear ch_MUA ch_tmp ch_tmp_MUA TimeStamps Trial_time_tmp
+            end
+            fclose(fidout);
+            output_file=[file_dir_output,'Trial_time_all_probe',num2str(probe_tmp),'shank',num2str(shank_tmp),'_trial_',num2str(Trial_time_all(2,1)),'_',num2str(Trial_time_all(2,end)),'.mat'];
+            save(output_file,'Trial_time_all','-v7.3');
+            events=find(Trial_time_all(1,:)==0)'/fs;
+            writematrix(events, fullfile(file_dir_output, 'events.csv'));
+        end
+    end
+return
+end
---
+++ master_kilosort_WuElectrode.m
@@ -0,0 +1,97 @@
+clear
+addpath(genpath('C:\Kilosort2-user\my_kilosort'));
+datapath={
+    'C:\Users\XinHao\Desktop\DiffLearn_MultiRegion\Ephys\CIBRXH027\2025-11-25_12-29-37\';... 
+    'C:\Users\XinHao\Desktop\DiffLearn_MultiRegion\Ephys\CIBRXH027\2025-11-26_10-05-43\';... 
+    'C:\Users\XinHao\Desktop\DiffLearn_MultiRegion\Ephys\CIBRXH027\2025-11-27_10-06-00\';... 
+    'C:\Users\XinHao\Desktop\DiffLearn_MultiRegion\Ephys\CIBRXH027\2025-11-28_12-27-55\';... 
+};
+
+for k = 1:length(datapath)
+    probe = 1; 
+    shank = 1; 
+    for probe_tmp=1:probe
+        for shank_tmp=1:shank
+            rootZ = [datapath{k},'Kilosort_data',num2str(probe_tmp),num2str(shank_tmp),'\']; %,num2str(position) the raw data binary file is in this folder
+            addpath(genpath('C:\Kilosort2-master')) % path to kilosort folder
+            addpath('C:\Kilosort2-user\my_kilosort\npy-matlab-master\npy-matlab') % for converting to Phy
+            rootH = rootZ; % path to temporary binary file (same size as data, should be on fast SSD)
+            pathToYourConfigFile = 'C:\Kilosort2-user\my_kilosort\configFiles'; % take from Github folder and put it somewhere else (together with the master_file)
+            chanMapFile = 'C:\Users\XinHao\Desktop\DiffLearn_MultiRegion\Ephys\WuElectrode_kilosortChanMap.mat';
+
+            ops.trange = [0 Inf]; % time range to sort
+            ops.NchanTOT    = 64; % total number of channels in your recording
+
+            % run(fullfile(pathToYourConfigFile, ['configFile32',num2str(shank_ch),'.m']))
+            ops.chanMap = fullfile(chanMapFile);
+            % ops.chanMap = 1:ops.Nchan; % treated as linear probe if no chanMap file
+            ops.fs = 20000;  % sample rate
+            ops.fshigh = 300; % frequency for high pass filtering
+            ops.minfr_goodchannels = 0.1;  % minimum firing rate on a "good" channel (0 to skip)
+            ops.Th = [10 4];  % threshold on projections (like in Kilosort1, can be different for last pass like [10 4])
+            ops.lam = 10;  % how important is the amplitude penalty (like in Kilosort1, 0 means not used, 10 is average, 50 is a lot) 
+            ops.AUCsplit = 0.9;  % splitting a cluster at the end requires at least this much isolation for each sub-cluster (max = 1)
+            ops.minFR = 1/50;  % minimum spike rate (Hz), if a cluster falls below this for too long it gets removed
+            ops.momentum = [20 400];  % number of samples to average over (annealed from first to second value) 
+            ops.sigmaMask = 30; % spatial constant in um for computing residual variance of spike
+            ops.ThPre = 8; % threshold crossings for pre-clustering (in PCA projection space)
+            % danger, changing these settings can lead to fatal errors
+            % options for determining PCs
+            ops.spkTh           = -4;%-6;      % spike threshold in standard deviations (-6)
+            ops.reorder         = 1;       % whether to reorder batches for drift correction. 
+            ops.nskip           = 25;  % how many batches to skip for determining spike PCs
+            ops.GPU                 = 1; % has to be 1, no CPU version yet, sorry
+            % ops.Nfilt               = 1024; % max number of clusters
+            ops.nfilt_factor        = 4; % max number of clusters per good channel (even temporary ones)
+            ops.ntbuff              = 64;    % samples of symmetrical buffer for whitening and spike detection
+            ops.NT                  = 64*1024+ ops.ntbuff; % must be multiple of 32 + ntbuff. This is the batch size (try decreasing if out of memory). 
+            ops.whiteningRange      = 32; % number of channels to use for whitening each channel
+            ops.nSkipCov            = 25; % compute whitening matrix from every N-th batch
+            ops.scaleproc           = 200;   % int16 scaling of whitened data
+            ops.nPCs                = 3; % how many PCs to project the spikes into
+            ops.useRAM              = 0; % not yet available
+            ops.fproc       = fullfile(rootH, 'temp_wh.dat'); % proc file on a fast SSD
+    
+            % channel map file check
+            fs = dir(fullfile(rootZ, 'chan*.mat'));
+            if ~isempty(fs)
+                ops.chanMap = fullfile(rootZ, fs(1).name);
+            end
+    
+            % find the binary file
+            fs          = [dir(fullfile(rootZ, '*.bin')) dir(fullfile(rootZ, '*.dat'))];
+            ops.fbinary = fullfile(rootZ, fs(1).name);
+    
+            % preprocess data to create temp_wh.dat
+            rez = preprocessDataSub(ops);
+    
+            % time-reordering as a function of drift
+            rez = clusterSingleBatches(rez);
+    
+            % saving here is a good idea, because the rest can be resumed after loading rez
+            save(fullfile(rootZ, 'rez.mat'), 'rez', '-v7.3');
+    
+            % main tracking and template matching algorithm
+            rez = learnAndSolve8b(rez);
+    
+            % final merges
+            rez = find_merges(rez, 1);
+    
+            % final splits by SVD
+            rez = splitAllClusters(rez, 1);
+    
+            % final splits by amplitudes
+            rez = splitAllClusters(rez, 0);
+    
+            % decide on cutoff
+            rez = set_cutoff(rez);
+            fprintf('found %d good units \n', sum(rez.good>0))
+    
+            % write to Phy
+            fprintf('Saving results to Phy  \n')
+            rezToPhy(rez, rootZ);
+    
+            clearvars -except datapath Laser_trial k position probe shank probe_tmp shank_tmp mmapN
+        end
+    end
+end
